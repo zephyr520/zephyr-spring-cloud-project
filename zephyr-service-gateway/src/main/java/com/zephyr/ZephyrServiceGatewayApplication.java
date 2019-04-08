7c6562f35be1eee3e0824595cb35b38f8d4a3c5f
zephyr520 <zephyr_520@163.com>
2019-04-08 18:26:52 +0800

    初始化Gateway

---
+++ zephyr-service-gateway/src/main/java/com/zephyr/ZephyrServiceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zephyr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZephyrServiceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZephyrServiceGatewayApplication.class, args);
+	}
+
+}
---
+++ zephyr-service-gateway/src/test/java/com/zephyr/ZephyrServiceGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zephyr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZephyrServiceGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
